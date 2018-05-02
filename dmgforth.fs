63369ba45af00e12c4703dad6c8cc502e8a70bb4
Tijn Kersjes <tijn@reaktor.fi>
2018-05-02 18:40:23 +0200

    Test passing

--- dmgforth.fs
+++ dmgforth.fs
@@ -1,6 +1,6 @@
 #! /usr/bin/env gforth
-( 
-dmgforth.fs --- 
+(
+dmgforth.fs ---
 )
 
 vocabulary dmgforth
@@ -9,24 +9,23 @@ dmgforth definitions
 : kB 1024 * ;
 
 32 kB constant rom-size
-create rom-base rom-size allot
+create rom-base
+
+
+
+
+include rom.fs
+
+
+
+
 : rom rom-base rom-size ;
 
-0 Value rom-fd 
+0 Value rom-fd
 : dump-rom ( c-addr u -- )
   s" ./output.gb" w/o bin create-file throw TO rom-fd
   rom rom-fd write-file throw
   rom-fd close-file throw ;
-
-rom erase
-
-\ Next, the GameBoy starts adding all of the bytes
-\ in the cartridge from $134 to $14d. A value of 25
-\ decimal is added to this total. If the least
-\ significant byte of the result is a not a zero,
-\ then the GameBoy will stop doing anything
-
-$ff rom-base 10 + c!
 
 \ main
 
