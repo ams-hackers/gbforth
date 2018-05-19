a67abf015fac8a988818ab8de57a6aa8edb1da80
Tijn Kersjes <tijn@reaktor.fi>
2018-05-20 01:28:03 +0300

    Move header modifying words to cartridge

--- src/cartridge.fs
+++ src/cartridge.fs
@@ -5,6 +5,14 @@ also gb-assembler
 
 ( Constants used in cartridge header )
 
+: logo
+  $ce rom, $ed rom, $66 rom, $66 rom, $cc rom, $0d rom, $00 rom, $0b rom,
+  $03 rom, $73 rom, $00 rom, $83 rom, $00 rom, $0c rom, $00 rom, $0d rom,
+  $00 rom, $08 rom, $11 rom, $1f rom, $88 rom, $89 rom, $00 rom, $0e rom,
+  $dc rom, $cc rom, $6e rom, $e6 rom, $dd rom, $dd rom, $d9 rom, $99 rom,
+  $bb rom, $bb rom, $67 rom, $63 rom, $6e rom, $0e rom, $ec rom, $cc rom,
+  $dd rom, $dc rom, $99 rom, $9f rom, $bb rom, $b9 rom, $33 rom, $3e rom, ;
+
 ( CGB flag [$0143] )
 $80 constant CGB_SUPPORTED
 $C0 constant CGB_ONLY
@@ -57,6 +65,40 @@ $02 constant RAM_SIZE_8KBYTE
 $03 constant RAM_SIZE_32KBYTE
 $04 constant RAM_SIZE_128KBYTE
 $05 constant RAM_SIZE_64KBYTE
+
+( Words to modify header values )
+
+: parse-line ( -- addr u )
+  #10 parse ;
+
+: title:
+  parse-line
+  dup #15 > abort" Title is too long"
+  $134 offset>addr swap move ;
+
+: manufacturer:
+  parse-line
+  dup #4 > abort" Manufacturer Code is too long"
+  $013F offset>addr swap move ;
+
+: licensee:
+  parse-line
+  dup #2 > abort" Licensee Code is too long"
+  $0144 offset>addr swap move
+  $33 $014B rom! ;
+
+: gbgame $00 $0143 rom! ; ( non color )
+
+
+: header-complement
+  0
+  $014D $0134 ?do
+    i rom@ +
+  loop
+  $19 + negate ;
+
+: fix-header-complement
+  header-complement $014D rom! ;
 
 ( Cartridge structure )
 
