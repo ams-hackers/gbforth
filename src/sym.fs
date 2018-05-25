db7405e823340320ffd7cc3504d8a930bcf7da7e
Tijn Kersjes <tijn@divbyzero.nl>
2018-05-26 00:41:55 +0300

    Simplify sym.fs

--- src/sym.fs
+++ src/sym.fs
@@ -1,58 +1,27 @@
 require ./utils.fs
 
-32 kB constant sym-size
-create sym-base sym-size allot
-variable sym-offset-variable
+s" output.sym" w/o create-file throw Value sym-out
 
-: sym-offset sym-offset-variable @ ;
-: sym-offset! sym-offset-variable ! ;
-: sym-offset+! sym-offset-variable +! ;
+: sym-cr
+  #10 sym-out emit-file throw ;
 
-: sym sym-base sym-size ;
+: sym-write ( c-addr u -- )
+  sym-out write-file throw
+  sym-out flush-file throw  ;
 
-: sym-offset>addr
-  sym-base + ;
+: addr-to-str ( nn -- c-addr u )
+  base @ >r
+  hex
+  0
+  <<#
+  # # # #
+  #>
+  #>>
+  r> BASE ! ;
 
-: sym@ ( offset -- val )
-  sym-offset>addr c@ ;
-
-: sym! ( val offset -- )
-  sym-offset>addr c! ;
-
-: sym,
-  sym-offset sym!
-  $1 sym-offset+! ;
-
-: sym-sp, $20 sym, ;
-: sym-nl, $0A sym, ;
-
-: sym-move ( addr u -- )
-  dup >r
-  sym-offset sym-offset>addr swap move
-  r> sym-offset+! ;
-
-: sym"
-  [char] " parse sym-move ;
-
-0 Value sym-fd
-: dump-sym ( c-addr u -- )
-  w/o create-file throw TO sym-fd
-  sym sym-fd write-file throw
-  sym-fd close-file throw ;
-
-: addr-to-str ( nn -- )
-  base @ >r      \ store original base
-  hex            \ switch to hex
-  0              \ convert to unsigned double
-  <<#            \ start conversion
-  # # # #        \ convert four least-significant digits
-  #>             \ complete conversion
-  #>>            \ release hold area
-  r> BASE ! ;    \ reset original base
-
-: add-to-sym, ( c-addr u nn -- )
-  s" 00:" sym-move
-  addr-to-str sym-move
-  sym-sp,
-  sym-move
-  sym-nl, ;
+: sym-emit ( c-addr u nn -- )
+  s" 00:"       sym-write
+  addr-to-str   sym-write
+  s"  "         sym-write
+  ( c-addr u )  sym-write
+  sym-cr ;
