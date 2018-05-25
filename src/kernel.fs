bd69e97d14909a2a662be19dc162457b7bc37963
Tijn Kersjes <tijn@divbyzero.nl>
2018-05-25 23:21:15 +0300

    Remove cycle annotation comments from kernel

--- src/kernel.fs
+++ src/kernel.fs
@@ -8,45 +8,37 @@ also gb-assembler
 ( TEMPORARY HACK: Don't break the game! )
 $4400 ==>
 
-\ : PS $FFFD -> $FF6F ;  \ Parameter Stack address
-\ : PSP C ;     \ Parameter Stack Pointer
-\ : (PSP) [C] ; \ TOS content
-
 : ps-clear,
-  $EF # C ld, ; ( init PSP to 0 )
+  $EF # C ld, ;
 
-( 8c/6b )
 : ps-push-hl,
-  C dec,        ( 1c/1b )
-  H A ld,       ( 1c/1b )
-  A [C] ld,     ( 2c/1b )
-  C dec,        ( 1c/1b )
-  L A ld,       ( 1c/1b )
-  A [C] ld, ;   ( 2c/1b )
+  C dec,
+  H A ld,
+  A [C] ld,
+  C dec,
+  L A ld,
+  A [C] ld, ;
 
-( 11c/9b )
 : ps-push-lit,
-  ps-push-hl,   ( 8c/6b )
-  ( nn ) # HL ld, ( 3c/3b )
+  ps-push-hl,
+  # HL ld,
 ;
 
-( 8c/6b )
 : ps-pop-hl,
-  [C] A ld,     ( 2c/1b )
-  A L ld,       ( 1c/1b )
-  C inc,        ( 1c/1b )
-  [C] A ld,     ( 2c/1b )
-  A H ld,       ( 1c/1b )
-  C inc, ;      ( 1c/1b )
+  [C] A ld,
+  A L ld,
+  C inc,
+  [C] A ld,
+  A H ld,
+  C inc, ;
 
-( 8c/6b )
 : ps-pop-de,
-  [C] A ld,     ( 2c/1b )
-  A E ld,       ( 1c/1b )
-  C inc,        ( 1c/1b )
-  [C] A ld,     ( 2c/1b )
-  A D ld,       ( 1c/1b )
-  C inc, ;      ( 1c/1b )
+  [C] A ld,
+  A E ld,
+  C inc,
+  [C] A ld,
+  A D ld,
+  C inc, ;
 
 ( Assume you have the following code
 
@@ -89,28 +81,24 @@ $4400 ==>
 : ps-push-lit, ps-push-lit, ;
 : ps-pop-hl, ps-pop-hl, ;
 
-( 26c/19b )
 code dup
-ps-push-hl,   ( 8c/6b )
-ret,          ( 4c/1b )
+ps-push-hl,
+ret,
 
-( 30c/20b )
 code +
-ps-pop-de,    ( 8c/6b )
-DE HL add,    ( 2c/1b )
-ret,          ( 4c/1b )
+ps-pop-de,
+DE HL add,
+ret,
 
-( 16c/7b -> 72c/46b )
 code double
-dup # call,   ( 6c/3b )
-+ # call,     ( 6c/3b )
-ret,          ( 4c/1b )
+dup # call,
++ # call,
+ret,
 
-( 16c/7b -> 160c/99b )
 code quadruple
-double # call,  ( 6c/3b )
-double # call,  ( 6c/3b )
-ret,            ( 4c/1b )
+double # call,
+double # call,
+ret,
 
 [end-user-definitions]
 
