d932298bac69a61b86468d27fd7cf06bdb517383
Tijn Kersjes <tijn@divbyzero.nl>
2018-05-26 21:07:28 +0200

    Generate sym filename automatically

--- src/sym.fs
+++ src/sym.fs
@@ -1,6 +1,29 @@
 require ./utils.fs
 
-s" output.sym" w/o create-file throw Value sym-out
+: append-to { c-addr u addr -- addr' }
+  c-addr addr u move
+  addr u + ;
+
+: append-string ( c-addr1 u1 c-addr2 u2 -- c-addr3 u3 )
+  2swap dup >r
+  pad append-to
+
+  over >r
+  append-to
+
+  drop pad r> r> + ;
+
+( Remove the extension of a filename )
+: sans-extname ( c-addr u -- c-addr' u' )
+  dup 1+ 1 ?do
+    2dup i - + c@
+    [char] . = if i - unloop exit then
+  loop ;
+
+: to-symbol-file ( c-addr u -- c-addr' u' )
+  sans-extname s" .sym" append-string ;
+
+output-file to-symbol-file w/o create-file throw Value sym-out
 
 : sym-cr
   #10 sym-out emit-file throw ;
