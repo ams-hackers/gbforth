72c48714927f87b84c1e3cef508be1848b83b476
Tijn Kersjes <tijn@divbyzero.nl>
2018-06-02 16:19:57 +0200

    Improve test for - primitive
    Add edge cases for 0 and negative result

--- test/test-minus.fs
+++ test/test-minus.fs
@@ -6,6 +6,14 @@ $abcd ps-push-lit,
 $1234 ps-push-lit,
 ' - # call,
 
+$89ab ps-push-lit,
+$89ab ps-push-lit,
+' - # call,
+
+$AAAA ps-push-lit,
+$AAAB ps-push-lit,
+' - # call,
+
 label loop
 halt,
 loop jr,
--- test/test-minus.js
+++ test/test-minus.js
@@ -2,5 +2,5 @@ const gb = require("./gbtest")(__filename);
 
 test("minus", () => {
   gb.cycles(200);
-  expect(gb.stack).toEqual([0x9999]);
+  expect(gb.stack).toEqual([0x9999, 0x0, 0xFFFF]);
 });
