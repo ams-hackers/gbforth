0e869fc6593c56f0d7b85f7854d90c35d6ed0202
Tijn Kersjes <tijn@divbyzero.nl>
2018-06-09 21:02:50 +0200

    Add dictionary pointer

--- src/kernel.fs
+++ src/kernel.fs
@@ -1,6 +1,7 @@
 ( A SUBROUTINE-THREADED KERNEL FOR DMG-FORTH )
 
 require ./asm.fs
+require ./utils/bytes.fs
 
 [asm]
 
@@ -36,6 +37,15 @@ require ./asm.fs
 
 $FFFE constant SP0 \ end of HRAM
 $CFFF constant RS0 \ end of RAM bank 0
+$C000 constant DP0 \ start of RAM bank 0
+
+variable DP
+DP0 2 + DP ! \ init dictionary pointer
+
+: dp-init,
+  DP @ dup
+  higher-byte # A ld, A DP0 ]* ld,
+  lower-byte # A ld, A DP0 1 + ]* ld, ;
 
 : ps-clear,
   SP0 $FF00 - # C ld, ;
