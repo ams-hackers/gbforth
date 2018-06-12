a6efc7dc87f417d07c5e8d688ad1209d74784cdf
David Vázquez Púa <davazp@gmail.com>
2018-06-12 08:22:19 +0200

    Improve command line argument processing

--- src/cli.fs
+++ src/cli.fs
@@ -1,19 +1,50 @@
 ( Add lib/ to the search path of the game )
 s" DMGFORTH_PATH" getenv 2constant dmgforth-path
 
-: usage
-  ." Usage: dmgforth [--no-kernel] <input> <output>" cr
+: process-exit
   1 (bye) ;
 
-1 arg s" --no-kernel" compare 0= [if]
-  true Value --no-kernel
-  shift-args
-[else]
-  false Value --no-kernel
-[then]
+: usage
+  ." Usage: dmgforth [--no-kernel] <input> <output>" cr ;
+
+false Value --no-kernel
+false Value --verbose
+
+: arg= 1 arg compare 0= ;
+
+: process-option
+  s" --no-kernel" arg= if
+    true To --no-kernel
+    shift-args
+    true exit
+  then 
+
+  s" --verbose" arg= if
+    true To --verbose
+    shift-args
+    true exit
+  then
+
+  s" --version" arg= if
+    ." dmgforth 0.1.0" CR
+    process-exit
+    true exit
+  then
+
+  s" --help" arg= if
+    usage
+    process-exit
+    true exit
+  then
+  
+  false 
+;
+
+[begin] process-option [while] [repeat]
 
 argc @ 3 <> [if]
   ' usage stderr outfile-execute
+  process-exit
 [then]
 
 next-arg 2constant input-file
