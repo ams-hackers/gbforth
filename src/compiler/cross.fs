b38af3c12f3be9a26b4c097312af49bd06f32d38
David Vázquez Púa <davazp@gmail.com>
2018-06-14 19:39:29 +0200

    Separate xname related code to compiler/xname.fs

--- src/compiler/cross.fs
+++ src/compiler/cross.fs
@@ -6,31 +6,7 @@ require ./ir.fs
 require ./code.fs
 require ./codegen.fs
 
-( Cross words )
-
-%001 constant F_IMMEDIATE
-%010 constant F_CONSTANT
-%100 constant F_PRIMITIVE
-
-struct
-  cell% field xname-flags
-  cell% field xname-addr
-end-struct xname%
-
-: make-xname ( addr flag -- xname )
-  >r >r
-  xname% %zalloc
-  r> over xname-addr !
-  r> over xname-flags ! ;
-
-: >xcode xname-addr @ ;
-: >xflags xname-flags @ ;
-: ximmediate? >xflags F_IMMEDIATE and 0<> ;
-: xconstant?  >xflags F_CONSTANT and 0<> ;
-: xprimitive?  >xflags F_PRIMITIVE and 0<> ;
-
-: or! ( u addr -- )
-  tuck @ or swap ! ;
+require ./xname.fs
 
 : emitted-addr
   dup xprimitive? if
---
+++ src/compiler/xname.fs
@@ -0,0 +1,23 @@
+( Cross words )
+
+%001 constant F_IMMEDIATE
+%010 constant F_CONSTANT
+%100 constant F_PRIMITIVE
+
+struct
+  cell% field xname-flags
+  cell% field xname-addr
+end-struct xname%
+
+: make-xname ( addr flag -- xname )
+  >r >r
+  xname% %zalloc
+  r> over xname-addr !
+  r> over xname-flags ! ;
+
+: >xcode xname-addr @ ;
+: >xflags xname-flags @ ;
+: ximmediate? >xflags F_IMMEDIATE and 0<> ;
+: xconstant?  >xflags F_CONSTANT and 0<> ;
+: xprimitive?  >xflags F_PRIMITIVE and 0<> ;
+
