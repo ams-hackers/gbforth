e22daf9cd4778868a59c978e65cbe8615b5beae5
Tijn Kersjes <tijn@divbyzero.nl>
2018-06-21 00:06:04 +0200

    Improve cmove test
    Verify that overlapping memory ranges are working correctly

--- test/core/test-cmove.fs
+++ test/core/test-cmove.fs
@@ -1,20 +1,16 @@
 ( initialize )
 : initialize
+  0 $C0FE c!
+  0 $C0FF c!
+  0 $C100 c!
   1 $C101 c!
   2 $C102 c!
   3 $C103 c!
   4 $C104 c!
   5 $C105 c!
   6 $C106 c!
-
-  0 $C201 c!
-  0 $C202 c!
-  0 $C203 c!
-  0 $C204 c!
-  0 $C205 c!
-  0 $C206 c!
 ;
 
 : main
   initialize
-  $C101 $C201 #5 cmove ;
+  $C101 $C0FF #5 cmove ;
--- test/core/test-cmove.js
+++ test/core/test-cmove.js
@@ -2,7 +2,7 @@ const gb = require("../gbtest")(__filename);
 
 test("cmove", () => {
   gb.cycles(1000);
-  const dest = Array.from(gb.memory).slice(0xc201, 0xc207);
-  expect(dest).toEqual([1, 2, 3, 4, 5, 0]);
+  const dest = Array.from(gb.memory).slice(0xc0fe, 0xc107);
+  expect(dest).toEqual([0, 1, 2, 3, 4, 5, 4, 5, 6]);
   expect(gb.depth).toBe(0);
 });
