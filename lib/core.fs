c2ed7462132afda75edb9bb24cb9c52880c339eb
David Vázquez Púa <davazp@gmail.com>
2018-06-24 00:40:29 +0200

    Remove duplicated conditionals
    This code is already in lib/core/conditionals.fs

--- lib/core.fs
+++ lib/core.fs
@@ -14,27 +14,6 @@ $FFFF constant true
 :m [R2] $FF81 ]* ;
 [endasm]
 
-( Conditionals )
-
-:m if ( -- alternative )
-  @there dup 0branch,
-; immediate
-
-:m else ( alternative -- continuation )
-  @there dup branch,
-  swap @resolve
-; immediate
-
-:m then ( continuation -- )
-  @resolve
-; immediate
-
-:m ahead ( -- orig )
-  @there dup branch,
-; immediate
-
-:m endif postpone then ; immediate
-
 (
   ***** Stack Manipulation *****
 )
