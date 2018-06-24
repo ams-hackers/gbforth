d554e15062a45819e2b9801cfa7a0a26fbcd08dc
David Vázquez Púa <davazp@gmail.com>
2018-06-24 10:50:32 +0200

    Move some logic from hello.fs to term.fs

--- examples/hello-world/hello.fs
+++ examples/hello-world/hello.fs
@@ -1,36 +1,17 @@
-require gbhw.fs
-require memory.fs
-require cpu.fs
-require lcd.fs
-
-require input.fs
-
 title: EXAMPLE
 gamecode: HELO
 makercode: RX
 
-create TileData
-include ibm-font.fs
+require gbhw.fs
+require input.fs
+require term.fs
 
 s" Hello World !"
 constant TitleLength
 constant TitleOffset
 
-: reset-palette
-  %11100100 rGBP c! ;
-
-: reset-window-scroll
-  0 rSCX c!
-  0 rSCY c! ;
-
-: copy-font
-  TileData _VRAM [ 256 8 * ]L cmovemono ;
-
-: clear-screen
-  _SCRN0 [ SCRN_VX_B SCRN_VY_B * ]L bl fill ;
-
 : copy-title
-  TitleOffset [ _SCRN0 3 + SCRN_VY_B 7 * + ]L TitleLength cmovevideo ;
+  TitleOffset TitleLength type ;
 
 ( program start )
 
@@ -51,12 +32,8 @@ constant TitleOffset
   again ;
 
 : main
-  disable-interrupts
-  reset-palette
-  reset-window-scroll
-  disable-lcd copy-font enable-lcd
-  clear-screen
-  copy-title
-  enable-interrupts
+  init-term
   init-input
+  page
+  copy-title
   handle-input ;
--- lib/term.fs
+++ lib/term.fs
@@ -1,6 +1,33 @@
+require ./cpu.fs
+require ./gbhw.fs
+require ./memory.fs
 
-create ibm-font
+create TileData
 include ./ibm-font.fs
 
-\ variable cursor
+: copy-font
+  TileData _VRAM [ 256 8 * ]L cmovemono ;
 
+: page
+  _SCRN0 [ SCRN_VX_B SCRN_VY_B * ]L bl fill ;
+
+: type ( addr u -- )
+  [ _SCRN0 3 + SCRN_VY_B 7 * + ]L swap cmovevideo ;
+
+: reset-palette
+  %11100100 rGBP c! ;
+
+: reset-window-scroll
+  0 rSCX c!
+  0 rSCY c! ;
+
+: init-term
+  disable-interrupts
+  reset-palette
+  reset-window-scroll
+
+  disable-lcd
+  copy-font
+  enable-lcd
+  enable-interrupts
+;
