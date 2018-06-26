9de525e013cf15530c856cf52e439cc0c1060a49
David Vázquez Púa <davazp@gmail.com>
2018-06-26 17:58:15 +0200

    Refactor ximmediate

--- src/compiler/xname.fs
+++ src/compiler/xname.fs
@@ -38,12 +38,12 @@ end-struct xname%
 : xconstant?  >xflags F_CONSTANT  and 0<> ;
 : xprimitive? >xflags F_PRIMITIVE and 0<> ;
 
-: ximmediate
-  latest name>string nextname
-  latestxt F_IMMEDIATE create-xname ;
-
 : ximmediate-as
   latestxt F_IMMEDIATE create-xname ;
+
+: ximmediate
+  latest name>string nextname
+  ximmediate-as ;
 
 : find-xname ( addr u -- xname )
   2>r
