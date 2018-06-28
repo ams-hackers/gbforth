7d4f9cdf9b86ff16c4ef3f6e9261f094ebff0d49
David Vázquez Púa <davazp@gmail.com>
2018-06-28 14:29:19 +0200

    Add test of separate DOES>

--- test/compiler/test-create-does.fs
+++ test/compiler/test-create-does.fs
@@ -1,10 +1,15 @@
 rom
 
 :m my-constant create , does> @ ;
+:m magic-constant does> drop $5432 ;
 
 $123 my-constant foo
+
+$123 my-constant bar
+magic-constant
 
 ram
 
 : main
-  foo ;
+  foo
+  bar ;
--- test/compiler/test-create-does.js
+++ test/compiler/test-create-does.js
@@ -2,5 +2,5 @@ const gb = require("../gbtest")(__filename);
 
 test("CREATE...DOES>", () => {
   gb.run();
-  expect(gb.stack).toEqual([0x123]);
+  expect(gb.stack).toEqual([0x123, 0x5432]);
 });
