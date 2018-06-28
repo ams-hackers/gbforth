1172190871ebf061ac7edd813a0800e8aafa9e2e
Tijn Kersjes <tijn@divbyzero.nl>
2018-06-28 21:13:19 +0300

    Add negative number test for mod

--- test/core/test-mod.js
+++ test/core/test-mod.js
@@ -2,5 +2,5 @@ const gb = require("../gbtest")(__filename);
 
 test("mod", () => {
   gb.run();
-  expect(gb.stack).toEqual([0, 11, 0, 3, 11, 0, 1]);
+  expect(gb.stack).toEqual([0, 11, 0, 3, 11, 0, 1, 9, 0, 1, 2, 0]);
 });
