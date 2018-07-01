ffec31975f70b646c7648f8f0c43a40effb1cd5d
David Vázquez Púa <davazp@gmail.com>
2018-07-01 11:39:39 +0200

    Add tests for sokoban game
    Add 2 tests, one visual, and another for pressing UP.
    
    This should give us some confidence in that we are not breaking the
    example without having to run it on the emulator.

--- test/test-examples.js
+++ test/test-examples.js
@@ -1,5 +1,27 @@
 const path = require("path");
 
+describe("sokoban", () => {
+  const gb = require("./gbtest")(
+    path.resolve(__dirname, "../examples/sokoban/sokoban.gb")
+  );
+
+  test("shows the first maze", () => {
+    gb.steps(40);
+    expect(gb.frameSha).toBe(
+      "1cfbf989e4053d75336160777db5dd56c385c1ccd7a0e47f5dc9a50e94b133ce"
+    );
+  });
+
+  test("The player moves up if UP is pressed", () => {
+    gb.steps(40);
+    gb.gameboy._joypad.keyDown(38);
+    gb.steps(10);
+    expect(gb.frameSha).toBe(
+      "5952414b1881adb2ad83f5ac410d2092b16b8508d9866c7b45e99be4afe722e5"
+    );
+  });
+});
+
 describe("hello-world", () => {
   const gb = require("./gbtest")(
     path.resolve(__dirname, "../examples/hello-world/hello.gb")
