56c648acddb9bff4b6f7a8ce0e672a7dcdb5ddb2
Tijn Kersjes <tijn@divbyzero.nl>
2020-05-17 17:56:28 +0200

    Add words to define interrupt handlers

--- examples/hello-world/hello.fs
+++ examples/hello-world/hello.fs
@@ -2,9 +2,19 @@ title: EXAMPLE
 
 require ibm-font.fs
 require term.fs
+require interrupt.fs
+
+variable num
+: num? 3 8 at-xy num c@ . space ;
+: num++ num c@ 1+ num c! ;
+
+p10-interrupt: num++
 
 : main
   install-font
   init-term
   3 7 at-xy
-  ." Hello World !" ;
+  ." Hello World !"
+  0 num !
+  enable-key-interrupts
+  begin num? again ;
